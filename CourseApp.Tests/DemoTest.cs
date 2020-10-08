ab789cba76d6a5907ae88a5edf922a4016cc13cf
CoolzGH <knyazke@gmail.com>
2020-10-08 23:29:44 +0300

    Some Tests for Math Lab

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -10,5 +10,46 @@ namespace CourseApp.Tests
         {
             Assert.True(true);
         }
+
+        [Theory]
+        [InlineData(0, 0, 0, double.NaN)]
+        [InlineData(1.1, 0.09, 1.2, 4.791)]
+        [InlineData(1, 1, 1, double.NaN)]
+        [InlineData(1, 0, 1, double.PositiveInfinity)]
+        public void TestCalcAllZeros(double a, double b, double x, double exp)
+        {
+            var actualResult = Program.Calc(a, b, x);
+            Assert.Equal(exp, actualResult, 3);
+        }
+
+        [Theory]
+        [InlineData(1.1, 0.09, 1.2, 2.2, 0.2, 6)]
+        public void TestTaskA(double a, double b, double xn, double xk, double dx, double exp)
+        {
+            var res = Program.TaskA(a, b, xn, xk, dx);
+            Assert.Equal(exp, res.Length);
+            double[] expX = { 1.2, 1.4, 1.6, 1.8, 2.0, 2.2 };
+            for (int i = 0; i <= 5; i++)
+            {
+                var (x, y) = res[i];
+                Assert.Equal(expX[i], x, 1);
+            }
+        }
+
+        [Fact]
+        public void TestTaskB()
+        {
+            int i = 0;
+            double[] xItems = { 1.21, 1.76, 2.53, 3.48, 4.52 };
+            var res = Program.TaskB(1.1, 0.09, xItems);
+            Assert.Equal(5, xItems.Length);
+            double[] expmas = { 5.865, 0.445, 0.501, 0.496, 0.489 };
+            foreach (var item in res)
+            {
+                var (x, y) = item;
+                Assert.Equal(expmas[i], y, 3);
+                i++;
+            }
+        }
     }
 }
