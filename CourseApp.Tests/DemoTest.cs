9525218184c8a5d183e8ab2553a5008898bea070
Anna2-42 <71084649+Anna2-42@users.noreply.github.com>
2020-10-09 15:49:36 +0300

    Add sample test cases

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -10,5 +10,29 @@ namespace CourseApp.Tests
         {
             Assert.True(true);
         }
+
+        [Theory]
+        [InlineData(0, 0, 0, double.NaN)]
+        [InlineData(0.8, 0.4, 1.23, 2.237)]
+        [InlineData(0, 1, 2, 2.508)]
+        [InlineData(0, -1, 0, double.NaN)]
+ public void TestCalcAllZeros(double a, double b, double x, double exp)
+        {
+            var actualResult = Program.Calc(a, b, x);
+            Assert.Equal(exp, actualResult, 3);
+        }
+
+        [Fact]
+        public void TestTaskA()
+        {
+            var res = Program.TaskA(0.8, 0.4, 1, 3, 1);
+            Assert.Equal(3, res.Length);
+            double[] expX = { 1, 2, 3 };
+            for (int i = 0; i <= 2; i++)
+            {
+                var (x, y) = res[i];
+                Assert.Equal(expX[i], x, 1);
+            }
+        }
     }
 }
