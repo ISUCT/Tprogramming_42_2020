3920103e535b80b9a1e017a00244834d784118df
Никита Павлов <you@example.com>
2020-10-09 13:23:52 +0300

    Add sample test cases

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -10,5 +10,31 @@ namespace CourseApp.Tests
         {
             Assert.True(true);
         }
+
+        [Theory]
+        [InlineData(0.1, 102.998)]
+        [InlineData(1, 15.022)]
+        [InlineData(1.5, double.NaN)]
+        [InlineData(-1, 996299.338)]
+        [InlineData(-5, double.NaN)]
+        [InlineData(0, 225.652)]
+        public void TestCalc(double x, double exp)
+        {
+            var actualResult = Program.Calc(x);
+            Assert.Equal(exp, actualResult, 3);
+        }
+
+        [Fact]
+        public void TestTaskA()
+        {
+            var res = Program.TaskA(0.26, 0.66, 0.08);
+            Assert.Equal(6, res.Length);
+            double[] expX = { 0.26, 0.34, 0.42, 0.5, 0.58, 0.66 };
+            for (int i = 0; i <= 5; i++)
+            {
+                var (x, y) = res[i];
+                Assert.Equal(expX[i], x, 1);
+            }
+        }
     }
 }
