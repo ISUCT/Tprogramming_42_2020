db36e637fc1f85a22c9e1659938a7b75eb876a1a
Nikita Chistjakov <nik-chistiackow2014@yandex.ru>
2020-10-09 17:05:03 +0300

    Programm

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,65 +4,66 @@ namespace CourseApp
 {
     public class Program
     {
-        public static double Calc(double a, double b, double x)
+                public static double Calc(double a, double b, double x)
         {
-            var numerator = Math.Log(Math.Pow(b, 2) - Math.Pow(x, 2)) / Math.Log(a);
-            var denominator = Math.Pow(Math.Abs(Math.Pow(x, 2) - Math.Pow(a, 2)), 1 / 3.0);
-            var y = numerator / denominator;
+            var chislitel = a + (b * x);
+            var znamenatel = Math.Pow(Math.Log(x), 3);
+            var y = Math.Pow(chislitel / znamenatel, 1 / 5.0);
             return y;
         }
 
-        public static (double x, double y)[] TaskA(double a, double b, double xn, double xk, double dx)
-        {
-            var res = new(double, double)[(int)Math.Ceiling((xk - xn) / dx) + 1];
-            int i = 0;
-            for (var x = xn; x <= xk; x += dx)
-            {
-                var y = Calc(a, b, x);
-                res[i] = (x, y);
-                i++;
-            }
+           public static (double x, double y)[] TaskA(double a, double b, double xn, double xk, double dx)
+           {
+               var res = new(double, double)[(int)Math.Ceiling((xk - xn) / dx) + 1];
+               int i = 0;
+               for (var x = xn; x <= xk; x += dx)
+               {
+                   var y = Calc(a, b, x);
+                   res[i] = (x, y);
+                   i++;
+                }
 
             return res;
-        }
-
-        public static (double x, double y)[] TaskB(double a, double b, double[] xItems)
-        {
-            var res = new(double, double)[xItems.Length];
-            int i = 0;
-            foreach (var x in xItems)
-            {
-                var y = Calc(a, b, x);
-                res[i] = (x, y);
-                i++;
             }
 
-            return res;
-        }
+           public static (double x, double y)[] TaskB(double a, double b, double[] xItems)
+           {
+               var res = new(double, double)[xItems.Length];
+               int i = 0;
+               foreach (var x in xItems)
+               {
+                   var y = Calc(a, b, x);
+                   res[i] = (x, y);
+                   i++;
+                }
 
-        public static void Main(string[] args)
-        {
-            const double a = 2.0;
-            const double b = 4.1;
-            Console.WriteLine($"--------- TASK A --------------");
-            var taskA = TaskA(a, b, 0.77, 1.77, 0.2);
-            foreach (var item in taskA)
-            {
-                var(x, y) = item;
-                Console.WriteLine($"x={x}, y={y}");
+               return res;
             }
 
-            Console.WriteLine($"--------- TASK B --------------");
-            double[] xItems = { 1.24, 1.38, 2.38, 3.21, 0.68 };
-            var taskB = TaskB(a, b, xItems);
-            foreach (var item in taskB)
-            {
-                var(x, y) = item;
-                Console.WriteLine($"x={x}, y={y}");
-            }
+           public static void Main(string[] args)
+           {
+               const double a = 7.2;
+               const double b = 1.3;
+               Console.WriteLine($"-------Task A---------");
+               var taskA = TaskA(a, b, 1.56, 4.71, 0.63);
+               foreach (var item in taskA)
+               {
+                   var(x, y) = item;
+                   Console.WriteLine($"x={x}, y={y}");
+                }
 
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-        }
+                Console.WriteLine($"-------Task B--------");
+                   double[] xItems = { 2.4, 2.8, 3.9, 4.7, 3.16 };
+                   var taskB = TaskB(a, b, xItems);
+                   foreach (var item in taskB)
+                   {
+                       var(x, y) = item;
+                       Console.WriteLine($"x={x}, y={y}");
+                   }
+
+               Console.WriteLine("Hello World!");
+               Console.WriteLine("Chistjakov Nikita");
+               Console.ReadLine();
+           }
     }
 }
