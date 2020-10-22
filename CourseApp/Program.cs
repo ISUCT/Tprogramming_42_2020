32fea8b598fb8755791ddc7fe9a12a349f883b4c
incriz <70756797+incriz@users.noreply.github.com>
2020-10-22 15:28:11 +0300

    first Math lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,73 +1,59 @@
 ﻿using System;
 
-namespace CourseApp
+namespace CourceApp
 {
     public class Program
     {
-        public static double Calc(double a, double b, double x)
+        public static double Func(double a, double b, double x)
         {
-            var numerator = Math.Log(Math.Pow(b, 2) - Math.Pow(x, 2)) / Math.Log(a);
-            var denominator = Math.Pow(Math.Abs(Math.Pow(x, 2) - Math.Pow(a, 2)), 1 / 3.0);
-            var y = numerator / denominator;
-            return y;
+            return Math.Pow(a + (b * x), 2.5) / (1 + Math.Log10(a + (b * x)));
         }
 
-        public static (double x, double y)[] TaskA(double a, double b, double xn, double xk, double dx)
+        public static (double x, double y)[] Task_a(double a, double b, double xn, double xk, double dx)
         {
-            if (xk > xn)
-            {
-                var res = new(double, double)[(int)Math.Ceiling((xk - xn) / dx) + 1];
-                int i = 0;
-                for (var x = xn; x <= xk; x += dx)
-                {
-                    var y = Calc(a, b, x);
-                    res[i] = (x, y);
-                    i++;
-                }
-
-                return res;
-            }
-
-            return new(double, double)[0];
-        }
-
-        public static (double x, double y)[] TaskB(double a, double b, double[] xItems)
-        {
-            var res = new(double, double)[xItems.Length];
+            var result = new(double, double)[(int)Math.Ceiling((xk - xn) / dx) + 1];
             int i = 0;
-            foreach (var x in xItems)
+            for (double x = xn; x <= xk; x += dx)
             {
-                var y = Calc(a, b, x);
-                res[i] = (x, y);
-                i++;
+                result[i++] = (x, Func(a, b, x));
             }
 
-            return res;
+            return result;
+        }
+
+        public static (double x, double y)[] Task_b(double a, double b, double[] items)
+        {
+            var result = new(double, double)[items.Length];
+            int i = 0;
+            foreach (var item in items)
+            {
+                result[i++] = (item, Func(a, b, item));
+            }
+
+            return result;
         }
 
         public static void Main(string[] args)
         {
-            const double a = 2.0;
-            const double b = 4.1;
-            Console.WriteLine($"--------- TASK A --------------");
-            var taskA = TaskA(a, b, 0.77, 1.77, 0.2);
-            foreach (var item in taskA)
+            const double a = 2.5;
+            const double b = 4.6;
+            const double xn = 1.1;
+            const double xk = 3.6;
+            const double dx = 0.5;
+            double[] mas = { 1.2, 1.28, 1.36, 1.46, 2.35 };
+            var result_A = Task_a(a, b, xn, xk, dx);
+            System.Console.WriteLine("Task_a");
+            foreach (var item in result_A)
             {
-                var (x, y) = item;
-                Console.WriteLine($"x={x}, y={y}");
+                System.Console.WriteLine($"x={item.x}  , y={item.y}");
             }
 
-            Console.WriteLine($"--------- TASK B --------------");
-            double[] xItems = { 1.24, 1.38, 2.38, 3.21, 0.68 };
-            var taskB = TaskB(a, b, xItems);
-            foreach (var item in taskB)
+            System.Console.WriteLine("Task_b");
+            var result_b = Task_b(a, b, mas);
+            foreach (var item in result_b)
             {
-                var (x, y) = item;
-                Console.WriteLine($"x={x}, y={y}");
+                System.Console.WriteLine($"x={item.x}  , y={item.y}");
             }
-
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
         }
     }
 }
