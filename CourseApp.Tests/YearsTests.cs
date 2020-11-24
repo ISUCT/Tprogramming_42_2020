8b42f22cace508862c19953cfaa3d09268dd9324
etogjesanya <Alexander.Al-M@yandex.ru>
2020-11-24 14:45:07 +0300

    Calculating_age

---
+++ CourseApp.Tests/YearsTests.cs
@@ -0,0 +1,42 @@
+using System;
+using Xunit;
+
+namespace CourseApp.Testsd
+{
+    public class YearsTests
+    {
+        [Fact]
+        public void TestBeforeTheDate ()
+        {
+            string exp = "Max: 16 лет, 7 месяцев и 1 дней.";
+            Student person = new Student();
+             person.Name = "Max";
+             DateTime pers = new DateTime(2004, 04, 23);
+             var ageP = Program.CalcAge(pers, person.Name);
+             Assert.Equal(ageP, exp);
+        }
+
+        [Fact]
+        public void TestTodayTheDate ()
+        {
+            string exp = "Max: 0 лет, С днём рождения!";
+            Student person = new Student();
+             person.Name = "Max";
+             DateTime pers = new DateTime(1258, 11, 12);
+             pers = DateTime.Today;
+             var ageP = Program.CalcAge(pers, person.Name);
+             Assert.Equal(ageP, exp);
+        }
+
+        [Fact]
+        public void TestAfterTheDate ()
+        {
+            string exp = "Max: 18 лет, 11 месяцев и 18 дней.";
+            Student person = new Student();
+             person.Name = "Max";
+             DateTime pers = new DateTime(2001, 12, 06);
+             var ageP = Program.CalcAge(pers, person.Name);
+             Assert.Equal(ageP, exp);
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -46,6 +46,43 @@ namespace CourseApp
             return res;
         }
 
+        public static string CalcAge(DateTime age, string person)
+        {
+            var year = DateTime.Now.Year - age.Year;
+            var month = 0;
+            var day = 0;
+            if (DateTime.Now.Month < age.Month)
+            {
+                year = year - 1;
+                var monthCount = DateTime.Now.Month - age.Month;
+                month = 12 + monthCount;
+            }
+            else
+            {
+                month = DateTime.Now.Month - age.Month;
+            }
+
+            if (DateTime.Now.Day < age.Day)
+            {
+                month = month - 1;
+                var dayCount = DateTime.Now.Day - age.Day;
+                day = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) + dayCount + 1;
+            }
+            else
+            {
+                day = DateTime.Now.Day - age.Day;
+            }
+
+            if ((month == 0) & (day == 0))
+            {
+                return $"{person}: {year} лет, С днём рождения!";
+            }
+            else
+            {
+                return $"{person}: {year} лет, {month} месяцев и {day} дней.";
+            }
+        }
+
         public static void Main(string[] args)
         {
             const double a = 1.6;
@@ -71,13 +108,17 @@ namespace CourseApp
             Console.WriteLine("Alexander Mokhnatkin");
             Console.WriteLine($"---------- Person ---------");
             Student st = new Student();
-            Employee emp = new Employee ();
+            Employee emp = new Employee();
             st.Name = "Max";
-            st.Age = 19;
+            st.Age = 18;
+            DateTime max = new DateTime(2001, 12, 06);
+            Console.WriteLine(CalcAge(max, st.Name));
             st.Temperament = "Phlegmatic";
             st.University = "ISUCT";
             emp.Name = "Alex";
-            emp.Age = 24;
+            emp.Age = 23;
+            DateTime alex = new DateTime(2004, 04, 23);
+            Console.WriteLine(CalcAge(alex, emp.Name));
             emp.Temperament = "Sanguine";
             emp.Company = "Microsoft";
             Console.WriteLine(st);
