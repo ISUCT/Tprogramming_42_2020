a82dd0489db016a90a7390413052eeef01e66993
CoolzGH <knyazke@gmail.com>
2020-12-03 17:43:17 +0300

    DataFix

--- CourseApp.Tests/AgeTest.cs
+++ CourseApp.Tests/AgeTest.cs
@@ -8,6 +8,7 @@ namespace CourseApp.Tests
         [Theory]
         [InlineData("egor", 2001, 9, 3, 2020, 12, 4, "Возраст egor: 19 лет, 3 месяцев, 2 дней")]
         [InlineData("max", 2001, 12, 6, 2020, 12, 4, "Возраст max: 18 лет, 11 месяцев, 30 дней")]
+        [InlineData("anton", 2001, 12, 6, 2020, 12, 6, "Возраст anton: 19 лет, 0 месяцев, 1 дней")]
         [InlineData("andrey", 2020, 12, 4, 2020, 12, 4, "Возраст andrey: 0 лет, 0 месяцев, 0 дней")]
         [InlineData("sanya", 2020, 12, 24, 2020, 12, 4, "Возраст sanya: введён не верно")]
         public void TestAgeCalc(string actname, int yearbirth, int monthbirth, int daybirth, int yeartoday, int monthtoday, int daytoday, string exp)
